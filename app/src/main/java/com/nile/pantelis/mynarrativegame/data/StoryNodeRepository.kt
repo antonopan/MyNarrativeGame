3881e3669f4a34027533d08a3b00c27cc12810e6
antonopan <pantelisantonopoulos11@gmail.com>
2025-08-11 08:40:25 -0700

    Simple Story Completed

--- app/src/main/java/com/nile/pantelis/mynarrativegame/data/StoryNodeRepository.kt
+++ app/src/main/java/com/nile/pantelis/mynarrativegame/data/StoryNodeRepository.kt
@@ -6,51 +6,51 @@ class StoryNodeRepository {
 
     private val storyNodeOne = StoryNode(
         name = "StoryNodeOne",
-        narrativePiece = "",
+        narrativePiece = "You got lost in the forest, and you find two paths. On the left you cannot see its end, on the right is a tent and a campfire.",
         optionOne = "Go Left",
         optionTwo = "Go Right"
     )
 
     private val storyNodeTwo = StoryNode(
         name = "StoryNodeTwo",
-        narrativePiece = "You went left and find an old bridge stretching across a misty ravine.",
-        optionOne = "Cross the bridge",
-        optionTwo = "Climb down into the ravine"
+        narrativePiece = "You follow the left path, but it does not seem to end. You are hungry and exhausted, but have a feeling that it is worth it.",
+        optionOne = "Keep going",
+        optionTwo = "Turn back"
     )
 
     private val storyNodeThree = StoryNode(
         name = "StoryNodeThree",
-        narrativePiece = "You went right and encounter a traveler who offers you a mysterious box.",
-        optionOne = "Accept the box",
-        optionTwo = "Politely decline and walk away"
+        narrativePiece = "You sit in the tent to rest. You feel that the tent has something inside.",
+        optionOne = "Relax for a bit and leave",
+        optionTwo = "Check inside"
     )
 
     private val storyNodeFour = StoryNode(
         name = "StoryNodeFour",
-        narrativePiece = "Crossing the bridge, you see a strange glowing figure ahead.",
-        optionOne = "Approach the figure",
-        optionTwo = "Hide and observe"
+        narrativePiece = "You see the end of the path. You can see your car, but not where you remembered it to be. You get inside, and you drive to what seems like home.",
+        optionOne = null,
+        optionTwo = null,
     )
 
     private val storyNodeFive = StoryNode(
         name = "StoryNodeFive",
-        narrativePiece = "Climbing into the ravine, you discover a cave entrance hidden by vines.",
-        optionOne = "Enter the cave",
-        optionTwo = "Continue following the ravine floor"
+        narrativePiece = "You find yourself back where you started. You start feeling weird things. You need to do something, and fast",
+        optionOne = "Go letF",
+        optionTwo = "gou rIgt"
     )
 
     private val storyNodeSix = StoryNode(
         name = "StoryNodeSix",
-        narrativePiece = "The traveler smiles as you accept the box. It starts to hum in your hands.",
-        optionOne = "Open the box",
-        optionTwo = "Throw it away"
+        narrativePiece = "You go in the tent and notice an old box. Inside you find a weird glowing marble",
+        optionOne = "Touch the marble",
+        optionTwo = "Get out of the tent"
     )
 
     private val storyNodeSeven = StoryNode(
         name = "StoryNodeSeven",
-        narrativePiece = "You walk away from the traveler and find a forked path through ancient ruins.",
-        optionOne = "Take the left path through the ruins",
-        optionTwo = "Take the right path into the forest"
+        narrativePiece = "You touch it. Immediately you start feeling pain. You start seeing visions. You want to reach out to them, but you cannot move.",
+        optionOne = null,
+        optionTwo = null
     )
 
     init {
@@ -77,28 +77,28 @@ class StoryNodeRepository {
         }
 
         StoryNodes["StoryNodeTwo"]?.let {
-            it.optionOnePath = storyNodeThree
+            it.optionOnePath = storyNodeFour
             it.optionTwoPath = storyNodeFive
         }
 
         StoryNodes["StoryNodeThree"]?.let {
-            it.optionOnePath = storyNodeTwo
+            it.optionOnePath = storyNodeFive
             it.optionTwoPath = storyNodeSix
         }
 
         StoryNodes["StoryNodeFour"]?.let {
-            it.optionOnePath = storyNodeFive
-            it.optionTwoPath = storyNodeSeven
-        }
-
-        StoryNodes["StoryNodeFive"]?.let {
             it.optionOnePath = null
             it.optionTwoPath = null
         }
 
+        StoryNodes["StoryNodeFive"]?.let {
+            it.optionOnePath = storyNodeTwo
+            it.optionTwoPath = storyNodeThree
+        }
+
         StoryNodes["StoryNodeSix"]?.let {
             it.optionOnePath = storyNodeSeven
-            it.optionTwoPath = storyNodeOne
+            it.optionTwoPath = storyNodeThree
         }
 
         StoryNodes["StoryNodeSeven"]?.let {
